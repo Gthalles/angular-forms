83108c8bf24dac7e4efe3d3f350103ccbbde94ea
Gthalles <thalles@gempe.dev>
2021-09-17 15:37:20 -0300

    Atribuindo ngForm ao formulário e fazendo a ligação entre os campos de input e o os atributos do objeto

--- src/app/template-driven/template-driven.component.ts
+++ src/app/template-driven/template-driven.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateDrivenComponent implements OnInit {
 
+  user: any = {};
+  
+  onSubmit(f: any) {
+    //3 ways to access the value
+    console.log(f.value);
+    console.log(this.user);
+    console.log(f.form.value);
+  }
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
 
 }
