201042f4eb4f810a8ce18ec1983442c29922dad1
Gthalles <thalles@gempe.dev>
2021-09-21 00:51:17 -0300

    Criando componente para auxiliar nos testes com o formulário

--- src/app/template-driven/template-driven.module.ts
+++ src/app/template-driven/template-driven.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { TemplateDrivenComponent } from './template-driven.component';
+import { FormDebugComponent } from '../form-debug/form-debug.component';
 
 
 
 @NgModule({
   declarations: [
-    TemplateDrivenComponent
+    TemplateDrivenComponent,
+    FormDebugComponent
   ],
   imports: [
     CommonModule,
