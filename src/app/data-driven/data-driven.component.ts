4e3b4ed7091e976f71d24d7be4420e839f8de9ca
Gthalles <thalles.garbelotti@g4tech.com.br>
2021-10-18 10:46:32 -0300

    Limpando codigo referente ao data-driven-form

--- src/app/data-driven/data-driven.component.ts
+++ src/app/data-driven/data-driven.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { createPipeDefinitionMap } from '@angular/compiler/src/render3/partial/pipe';
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-data-driven',
@@ -11,13 +10,14 @@ import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms'
 export class DataDrivenComponent implements OnInit {
   form!: FormGroup;
 
-  validAndTouched(inputField: any) {
+  validInput(inputField: any) {
     let field: any = this.form.get(inputField);
 
-    if (field?.invalid && field.untouched) // Retorna falso mesmo com o campo inválido pois o mesmo ainda não foi focado
-      return false;
-    else if (field?.invalid && field.touched) // Única possibilidade para mostrar erros: campo inválido e tocado (focado em algum momento)
-      return true;
+    if (field?.invalid && field.untouched) {
+      return false;   // Retorna falso mesmo com o campo inválido pois o mesmo ainda não foi focado
+    }
+    else if (field?.invalid && field.touched)
+      return true;    // Única possibilidade para mostrar erros: campo inválido e tocado (focado em algum momento)
     else
       return false;
   }
@@ -25,34 +25,23 @@ export class DataDrivenComponent implements OnInit {
   verifyEmail() {
     let emailField = this.form?.get('email');
 
-    if (emailField?.errors)
+    if (emailField?.errors) {
       return emailField?.errors.required && emailField?.touched;
+    }
+
     return false;
   }
 
   applyErrorStyle(inputField: any) {
     return {
-      'has-error': this.validAndTouched(inputField),
-      'has-feedback': this.validAndTouched(inputField)
+      'has-error': this.validInput(inputField),
+      'has-feedback': this.validInput(inputField)
     }
   }
 
-  reset() {
-    this.form?.reset();
-  }
-
-  constructor(
-    private formBuilder: FormBuilder,
-    private http: HttpClient
-  ) { }
+  constructor(private formBuilder: FormBuilder, private http: HttpClient) { }
 
   ngOnInit(): void {
-    /*
-    this.form? = new FormGroup({
-      name: new FormControl(null),
-      email: new FormControl(null),
-    });
-    */
     this.form = this.formBuilder.group({
       name: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(25)]],
       email: [null, [Validators.required, Validators.pattern("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?")]],
@@ -69,25 +58,23 @@ export class DataDrivenComponent implements OnInit {
   }
 
   onSubmit(form: FormGroup) {
-    console.log(this.form);
-
+    console.log(this.form.value);
+    // Método de requisição http para envio do formulário
     this.http.post('https://httpbin.org/post', JSON.stringify(this.form?.value))
       .subscribe((data: any) => {
         console.log(data);
-      },
-        (error: any) => {
+      },(error: any) => {
           alert("Erro");
-        })
+      })
   }
 
-  // FUNÇÕES REFERENTES AO ENDEREÇO/CEP
+  // Funções referentes ao Endereço
   searchCEP() {
     let cep: string = this.form.get('address.cep')?.value;
     cep.replace(/\D/g, '');
 
     if (cep != "") {
-      //Expressão regular para validar o CEP.
-      var validateCep = /^[0-9]{8}$/;
+      var validateCep = /^[0-9]{8}$/;   // Expressão regular para validar o CEP
 
       if (validateCep.test(cep)) {
         this.http.get('https://viacep.com.br/ws/' + cep + '/json/')
@@ -105,7 +92,7 @@ export class DataDrivenComponent implements OnInit {
   populateForm(data: any) {
     this.form.patchValue({
       address: {
-        cep: data.cep, //postal code
+        cep: data.cep,    //postal code
         complement: data.complemento,
         street: data.logradouro,
         neighborhood: data.bairro,
@@ -113,8 +100,23 @@ export class DataDrivenComponent implements OnInit {
         state: data.uf
       }
     })
-    if(this.form.get('address.cep')?.value == "18870-003")
-      this.form.get('name')?.setValue("thisform.get('inputName').setValue()");
+
+    // Settando valor caso o cep seja o da minha rua
+    if(this.form.get('address.cep')?.value == "18870-003") {
+      this.form.patchValue({
+        name: 'G4 thalles',
+        email: 'thalles.garbelotti@g4tech.com.br',
+        address: {
+          number: '184'
+        }
+      })
+    }
+    /*
+    // Settando valores do data-driven utilizando setValue()
+      this.form.get('name')?.setValue("G4 Thalles");
+      this.form.get('address.number')?.setValue("184");
+      this.form.get('email')?.setValue("");
+    */
   }
 
   clearAddress() {
