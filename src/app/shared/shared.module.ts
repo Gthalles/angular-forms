d98028882393a6c6a1b41edd55373a90b528fa08
Gthalles <thalles.garbelotti@g4tech.com.br>
2021-11-12 23:10:18 -0300

    Organizando modulo compartilhado

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,17 +1,18 @@
-import { InputControlErrorsComponent } from './input-control-errors/input-control-errors.component';
-import { FormDebugComponent } from './form-debug/form-debug.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { InputControlErrorsComponent } from './input-control-errors/input-control-errors.component';
+import { FormDebugComponent } from './form-debug/form-debug.component';
 import { ErrorMessageComponent } from './error-message/error-message.component';
 import { InputFieldComponent } from './input-field/input-field.component';
-import { FormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [
-    FormDebugComponent,
     InputControlErrorsComponent,
+    FormDebugComponent,
     ErrorMessageComponent,
     InputFieldComponent
   ],
@@ -20,8 +21,8 @@ import { FormsModule } from '@angular/forms';
     FormsModule
   ],
   exports: [
-    FormDebugComponent,
     InputControlErrorsComponent,
+    FormDebugComponent,
     ErrorMessageComponent,
     InputFieldComponent
   ]
